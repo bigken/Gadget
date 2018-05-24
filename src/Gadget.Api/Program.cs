2b38ce865b8d81e950ce64947c129a345980569f
ken wang <ken.wang@ef.com>
2018-05-24 16:04:29 +0800

    dock expose specify port ,not only 80

--- src/Gadget.Api/Program.cs
+++ src/Gadget.Api/Program.cs
@@ -1,16 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-
-namespace Gadget.Api
+﻿namespace Gadget.Api
 {
+    using System;
+    using Microsoft.AspNetCore;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Logging;
+
     public class Program
     {
         public static void Main(string[] args)
